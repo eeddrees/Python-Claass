44f621a902fcdd5b94deba69f893d6a6f05e0144
eeddrees <113112306+eeddrees@users.noreply.github.com>
2022-12-23 13:57:29 +0300

    Update circle.py

--- figures/circle.py
+++ figures/circle.py
@@ -1,1 +1,12 @@
 
+p = 3.1415
+
+def circle_area(default_radius = 5):
+    Area = (p *  default_radius ** 2)
+    print(Area)
+    return
+    
+def circle_perimeter(default_radius = 5):
+    circum_ = 2 * p * default_radius
+    print(circum_)
+    return
