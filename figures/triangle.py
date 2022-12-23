8368027f43686bbcd9f543bc1e3457c760c80266
eeddrees <113112306+eeddrees@users.noreply.github.com>
2022-12-23 13:58:41 +0300

    Update triangle.py

--- figures/triangle.py
+++ figures/triangle.py
@@ -1,1 +1,13 @@
-
+def triangle_perimeter(a = 7, b = 2, c = 8):
+    perim_ = a + b + c
+    print(perim_)
+    return
+    
+    
+    
+def triangle_area(a = 7, b = 2, c = 8):
+    S = (a + b + c)/2
+    A = (S - a) * (S - b) * (S - c) * S
+    A = A**0.5
+    print(A)
+    return
