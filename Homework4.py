df22383dab40b1d8f576834ad37465b656a1b19a
eeddrees <113112306+eeddrees@users.noreply.github.com>
2022-12-25 16:06:03 +0300

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -49,24 +49,3 @@ def keys_(a,y):
 a = {'a':'HELLO', 'b': 'WORLD'}
 keys_(a,'b')
 
-#ЗАД 5
-    
-from figures import *
-
-circle_area()
-circle_area(7)
-
-circle_perimeter()
-circle_perimeter(9)
-
-triangle_perimeter()
-triangle_perimeter(4,3,7)
-
-triangle_area()
-triangle_area(1,9,6)
-
-square_perimeter()
-square_perimeter(2)
-
-square_area()
-square_area(7)     
